fe26b7d499bd1ace1aacafa27383a1bc43307e7c
SmallPotatoIT <1148563917@qq.com>
2017-05-14 01:32:24 +0800

    gulp

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -2,17 +2,33 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 
 gulp.task('build', function(){
-  return browserify({entries: './src/app.js', debug: true})
+  return browserify({entries: './src/**/*.js', debug: true})
           .transform('babelify', {presets: ['es2015']})
           .bundle()
           .pipe(source('app.js'))
+          .pipe(buffer())
+          .pipe(sourcemaps.init())
+          .pipe(uglify())
+          .pipe(sourcemaps.write('./maps'))
           .pipe(gulp.dest('./dist/js'));
 });
 
 gulp.task('watch', ['build'], function(){
-  gulp.watch('./src/js/*.js', ['build']);
+  browserSync.init({
+    server: {
+      baseDir: '../dist/',
+      proxy: '3001'
+    }
+  });
+
+  gulp.watch('../app/*.html').on('change', reload);
 });
 
 gulp.task('default', ['build', 'watch']);
--- gulp/src/app.js
+++ gulp/src/app.js
@@ -1,1 +1,3 @@
 var test = require('./js/test.js');
+
+const num = 1000;
